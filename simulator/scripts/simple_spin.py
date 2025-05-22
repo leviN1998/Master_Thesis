f5f62ca9088f32b481c57fb2306d5b823a3b9abd
Levin Kolmar <levin.kolmar@googlemail.com>
2025-05-22 12:35:47 +0200

    fixed light issue

--- simulator/scripts/simple_spin.py
+++ simulator/scripts/simple_spin.py
@@ -23,9 +23,10 @@ from event_display import EventDisplay
 
 
 # set up paths
-object_name = "basic_cube.blend"
+object_name = "ball.blend"
 path_scene = os.path.abspath(os.getcwd()) + "/../data/scenes/" + object_name
 path_output = os.path.abspath(os.getcwd()) + "/../data/output/"
+output_name = "spinning_ball"
 path_image = path_output + "render_img.png"
 path_log = os.path.abspath(os.getcwd()) + "/../data/logs/"
 
@@ -43,7 +44,7 @@ ball.scale = (1, 1, 1)
 if "Light" in bpy.data.objects:
     light = bpy.data.objects['Light']
     light.location = Vector((5, 5, 5))
-    light.data.energy = 5
+    light.data.energy = 10
 
     light.data.type = 'SUN'
 
@@ -55,10 +56,10 @@ scene.render.fps = 10
 
 # test with axis-angle rotation
 ball.rotation_mode = 'AXIS_ANGLE'
-ball.rotation_axis_angle = (0, 0, 0, -1)
+ball.rotation_axis_angle = (0, 1, 1, -1)
 ball.keyframe_insert(data_path="rotation_axis_angle", frame=0, index=-1)
 
-ball.rotation_axis_angle = (np.pi*3, 0, 0, -1)
+ball.rotation_axis_angle = (np.pi*3, 1, 1, -1)
 ball.keyframe_insert(data_path="rotation_axis_angle", frame=200, index=-1)
 
 # Set interpolation to linear for constant rotation speed
@@ -74,27 +75,31 @@ render_res_x = 1920
 render_res_y = 1080
 scene.render.resolution_percentage = 100  # Ensure full resolution
 
+bpy.context.scene.view_settings.view_transform = "Standard" # Hopefully fix error
+
 # delete default camera
 if "Camera" in bpy.data.objects:
     cam_pos = bpy.data.objects['Camera'].location
     cam_rot = bpy.data.objects['Camera'].rotation_euler
-    bpy.data.objects['Camera'].select_set(True)
-    bpy.ops.object.delete()
+    cam_focal = bpy.data.objects['Camera'].data.lens
+    # bpy.data.objects['Camera'].select_set(True)
+    # bpy.ops.object.delete()
 
 # set up event camera
 ppsee = Blender_DvsSensor("Sensor")
+ppsee.cam = bpy.data.objects["Camera"]
 ppsee.set_sensor(nx=render_res_x, ny=render_res_y, pp=0.015)
 ppsee.set_dvs_sensor(th_pos=0.15, th_neg=0.15, th_n=0.05, lat=500, tau=300, jit=100, bgn=0.0001)
-ppsee.set_sensor_optics(1)
+ppsee.set_sensor_optics(cam_focal)
 master_collection = bpy.context.collection
 
 
-master_collection.objects.link(ppsee.cam)
+# master_collection.objects.link(ppsee.cam)
 scene.camera = ppsee.cam
 
 ppsee.set_position(cam_pos)
 ppsee.set_angle(cam_rot)
-ppsee.set_speeds([0.0, 0, 0], [0.0, 0.0, 10])
+# ppsee.set_speeds([0.0, 0, 0], [0.0, 0.0, 10])
 
 ppsee.init_tension()
 ppsee.init_bgn_hist("../data/noise/noise_pos_161lux.npy", "../data/noise/noise_pos_161lux.npy")
@@ -112,39 +117,46 @@ sys.stdout.flush()
 os.close(sys.stdout.fileno())
 fd = os.open(logfile, os.O_WRONLY)
 
-# Initialize video writer
-fourcc = cv2.VideoWriter_fourcc("M", "J", "P", "G")
-video = cv2.VideoWriter(path_output + "spinning_cube.avi", fourcc, 20.0, (ppsee.def_x, ppsee.def_y))
+render = True
+if render:
 
-ev = EventBuffer(0)
-ed = EventDisplay("Events", ppsee.def_x, ppsee.def_y, 10000)
+    # Initialize video writer
+    fourcc = cv2.VideoWriter_fourcc("M", "J", "P", "G")
+    video = cv2.VideoWriter(path_output + output_name + ".avi", fourcc, 20.0, (ppsee.def_x, ppsee.def_y))
 
-# Render loop for each frame
-for frame in pbar.tqdm(range(scene.frame_start, scene.frame_end + 1), desc="Rendering frames"):
-    scene.frame_set(frame)
+    ev = EventBuffer(0)
+    # ed = EventDisplay("Events", ppsee.def_x, ppsee.def_y, 10000)
 
-    ppsee.update_time(1 / 1000)
-    ppsee.print_position()
-    
-    bpy.ops.render.render(write_still=True)
 
-    # Read the rendered image
-    img = cv2.imread(path_image)
+    # Render loop for each frame
+    for frame in pbar.tqdm(range(scene.frame_start, scene.frame_end + 1), desc="Rendering frames"):
+        scene.frame_set(frame)
 
-    if frame == 0:
-        ppsee.init_image(img)
-    else:
-        pk = ppsee.update(img, 1000)
-        ed.update(pk, 1000)
-        ev.increase_ev(pk)
-        bpy.data.objects['Light'].data.energy += 0.01
+        ppsee.update_time(1 / 1000)
+        ppsee.print_position()
 
-    # Write the image to the video file
-    video.write(img)
+        bpy.ops.render.render(write_still=True)
 
-# Release the video writer
-video.release()
-ev.write(path_output + "spinning_cube_events.dat")
+        # Read the rendered image
+        img = cv2.imread(path_image)
+
+        if frame == 0:
+            ppsee.init_image(img)
+        else:
+            pk = ppsee.update(img, 1000)
+            # ed.update(pk, 1000)
+            ev.increase_ev(pk)
+            bpy.data.objects['Light'].data.energy += 0.01
+
+        # Write the image to the video file
+        video.write(img)
+
+    # Release the video writer
+    video.release()
+    ev.write(path_output + output_name + ".dat")
+
+# Write scene as well
+bpy.ops.wm.save_as_mainfile(filepath=path_output + output_name + ".blend")
 
 # disable output redirection
 os.close(fd)
