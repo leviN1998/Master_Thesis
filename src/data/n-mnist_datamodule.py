b6b23ac39cd0bdd3bead6a1adaaaf8ed3727c011
Levin Kolmar <levin.kolmar@googlemail.com>
2025-07-04 15:53:47 +0200

    preprocessing nmnist

--- src/data/n-mnist_datamodule.py
+++ src/data/n-mnist_datamodule.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, Optional, Tuple
 
+from cv2 import transform
 import torch
 from lightning import LightningDataModule
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
@@ -17,12 +18,12 @@ class N_MNISTDataModule(LightningDataModule):
     """
 
     def __init__(
-            self,
-            data_dir: str = "data/",
-            train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
-            batch_size: int = 64,
-            num_workers: int = 0,
-            pin_memory: bool = False,
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
     ) -> None:
         """Initialize a `N_MNISTDataModule`.
 
@@ -43,8 +44,8 @@ class N_MNISTDataModule(LightningDataModule):
         # taken from https://tonic.readthedocs.io/en/latest/getting_started/nmnist.html
         sensor_size = tonic.datasets.NMNIST.sensor_size
         self.transforms = transforms.Compose([
-                tonic.transforms.Denoise(filter_time=10000),
-                tonic.transforms.ToFrame(sensor_size=sensor_size, num_bins=3)
+                # tonic.transforms.Denoise(filter_time=10000),
+                tonic.transforms.ToFrame(sensor_size=sensor_size, n_time_bins=3)
         ])
 
         self.data_train: Optional[Dataset] = None
@@ -53,4 +54,118 @@ class N_MNISTDataModule(LightningDataModule):
 
         self.batch_size_per_device = batch_size
 
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of N-MNIST classes (10).
+        """
+        return 10
     
+    def prepare_data(self) -> None:
+        """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
+        within a single process on CPU, so you can safely add your downloading logic within. In
+        case of multi-node training, the execution of this hook depends upon
+        `self.prepare_data_per_node()`.
+
+        Do not use it to assign state (self.x = y).
+        """
+        tonic.datasets.NMNIST(self.hparams.data_dir, train=True)
+        tonic.datasets.NMNIST(self.hparams.data_dir, train=False)
+
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            trainset = tonic.datasets.NMNIST(self.hparams.data_dir, train=True, transform=self.transforms)
+            testset = tonic.datasets.NMNIST(self.hparams.data_dir, train=False, transform=self.transforms)
+            dataset = ConcatDataset(datasets=[trainset, testset])
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+    
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+    
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+    
+    def teardown(self, stage: Optional[str] = None) -> None:
+        """Lightning hook for cleaning up after `trainer.fit()`, `trainer.validate()`,
+        `trainer.test()`, and `trainer.predict()`.
+
+        :param stage: The stage being torn down. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+            Defaults to ``None``.
+        """
+        pass
+
+    def state_dict(self) -> Dict[Any, Any]:
+        """Called when saving a checkpoint. Implement to generate and save the datamodule state.
+
+        :return: A dictionary containing the datamodule state that you want to save.
+        """
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint. Implement to reload datamodule state given datamodule
+        `state_dict()`.
+
+        :param state_dict: The datamodule state returned by `self.state_dict()`.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
