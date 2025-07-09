2200b5543f65345caf7a8460ddfd64f25676c62c
Levin Kolmar <levin.kolmar@googlemail.com>
2025-07-09 15:46:57 +0200

    subcomponents fro firenet

--- src/models/components/subcomponents.py
+++ src/models/components/subcomponents.py
@@ -36,3 +36,50 @@ class ConvGRU(nn.Module):
         h_tilde = torch.tanh(self.out_gate(torch.cat([x, r_t * hidden], dim=1)))
         h_t = (1 - z_t) * hidden + z_t * h_tilde  # New hidden state
         return h_t
+    
+
+
+class ResidualBlock(nn.Module):
+    """ A residual Block for Firenet
+    
+        As used for the Firenet Paper and expalined in the paper (Deep Residual Learning for Image Recognition)
+
+        input ------>
+          |          |
+        Conv2d       |
+          |          |
+        ReLu         |
+          |          |
+        Conv2d       |
+          |          |
+          + <--------|
+          |
+        ReLu
+          |
+        Output
+
+        Note: if different input and output chnannels desired this class needs to be modified
+    """
+
+    def __init__(self, input_channels: int, kernel_size: int = 3):
+        super().__init__()
+        self.input_channels = input_channels
+        padding = kernel_size // 2  # to keep sizes
+
+        self.conv1 = nn.Conv2d(input_channels, input_channels, kernel_size, padding=padding)
+        self.conv2 = nn.Conv2d(input_channels, input_channels, kernel_size, padding=padding)
+
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Forward pass through the Residual Block.
+        
+        """
+        residual = x  # Save the input for the residual connection
+        
+        out = self.conv1(x)  
+        out = torch.relu(out)  
+        out = self.conv2(out)  
+
+        out += residual  
+        out = torch.relu(out)  
+        return out 
