285a136f10fe7b62b0c77b9c8ff6880b6ddcc983
Levin Kolmar <levin.kolmar@googlemail.com>
2025-08-12 10:19:33 +0200

    rewrite simulator 2/2

--- src/simulator/simulator2.py
+++ src/simulator/simulator2.py
@@ -1,8 +1,16 @@
 import bpy
+import bpy_extras
 import cv2
 import time
 import os
+import sys
 import numpy as np
+import pandas as pd
+sys.path.append("src/utils")
+sys.path.append("src/utils/IEBCS")
+sys.path.append("../src/utils/IEBCS") # debug
+sys.path.append("../src/utils")       # debug
+import rotations
 import eventIO
 from dvs_sensor import *
 from dvs_sensor_blender import Blender_DvsSensor
@@ -19,7 +27,11 @@ class Simulator:
         self.tmp_path = self.dataset_path + f"tmp/pid_{self.pid}/image_tmp.png"
         self.output_name = self.dataset_path + f"data/{self.num_string}/{self.num_string}_"
         self.scene_path = self.dataset_path + f"config/scene.blend"
+        self.coords_path = self.output_name + "ball_coords.csv"
+        self.metadata_path = self.output_name + "metadata.csv"
+        self.gt_path = self.output_name + "ground_truth.csv"
         self.ball_coords = []
+        self.calculate_fps()
 
         try:
             os.mkdir(self.dataset_path + "data/" + self.num_string)
@@ -28,8 +40,25 @@ class Simulator:
 
 
     def set_config(self, config):
-        
-        
+        self.generate_video = config["generate_video"]
+
+        self.dataset_path = config["dataset_path"]
+
+        o = config["initial_orientation"]
+        self.initial_orientation = rotations.Rotation()
+        self.initial_orientation.set_axis(o[0], o[1], o[2])
+        self.spin_axis = config["spin_axis"]
+        self.spin = rotations.Rotation()
+        self.spin.set_axis_np(np.array(self.spin_axis))
+        self.total_rotations = config["total_rotations"]
+
+        self.ball_start = config["ball_start"]
+        self.ball_end = config["ball_end"]
+        self.ball_scale_start = config["scale_start"]
+        self.ball_scale_end = config["scale_end"]
+
+        self.total_frames = config["total_frames"]
+        self.simulation_time = config["simulation_time"]
         self.video_fps = config["video_fps"]
         self.simulation_samples = config["simulation_samples"]
         self.ball_name = config["ball_name"]
@@ -48,6 +77,14 @@ class Simulator:
         self.jit = config["jit"]
         self.bgn = config["bgn"]
         self.ref_period = config["ref_period"]
+
+
+    def calculate_fps(self):
+        """
+        Calculate the frames per second (fps) based on the simulation time and number of frames.
+        """
+        self.video_length = self.simulation_time / 1000000.0  # convert microseconds to seconds
+        self.fps = int(self.total_frames / self.video_length)
 
 
     def init_scene(self):
@@ -93,10 +130,14 @@ class Simulator:
         self.event_camera.init_bgn_hist(self.dataset_path + "noise/noise_pos_161lux.npy", self.dataset_path + "noise/noise_pos_161lux.npy")
 
 
-    def generate_spin_keyframes(self):
+    def apply_initial_rotation(self):
+        """
+        Apply initial rotation to the ball.
+        The initial rotation is defined in the config file.
+        """
         self.ball.rotation_mode = 'AXIS_ANGLE'
-        ax = self.initial_rot.get_axis()
-        angle = self.initial_rot.get_angle()
+        ax = self.initial_orientation.get_axis()
+        angle = self.initial_orientation.get_angle()
         self.logger.debug(f"Initial rotation axis: {ax} with {angle} deg.")
         angle = angle * np.pi / 180.0 # convert to radians
         self.ball.rotation_axis_angle = (angle, ax[0], ax[1], ax[2])
@@ -104,30 +145,153 @@ class Simulator:
         bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
 
 
+    def generate_spin_keyframes(self):
+        """
+        
+        """
+        self.ball.rotation_mode = 'AXIS_ANGLE'
+        ax = self.spin.get_axis()
+        self.ball.rotation_axis_angle = (0, ax[0], ax[1], ax[2])
+        self.ball.keyframe_insert(data_path="rotation_axis_angle", frame=0, index=-1)
+
+        self.ball.rotation_axis_angle = (self.total_rotations * np.pi * 2, ax[0], ax[1], ax[2])
+        self.ball.keyframe_insert(data_path="rotation_axis_angle", frame=self.total_frames, index=-1)
+
+
+    def generate_scale_keyframes(self):
+        """ Generate keyframes for the ball scale
+        """
+        self.ball.scale = (self.ball_scale_start, self.ball_scale_start, self.ball_scale_start)
+        self.ball.keyframe_insert(data_path="scale", frame=0)
+
+        self.ball.scale = (self.ball_scale_end, self.ball_scale_end, self.ball_scale_end)
+        self.ball.keyframe_insert(data_path="scale", frame=self.total_frames)
 
 
     def generate_position_keyframes(self):
-        pass
+        self.ball.location = self.ball_start
+        self.ball.keyframe_insert(data_path="location", frame=0)
+
+        self.ball.location = self.ball_end
+        self.ball.keyframe_insert(data_path="location", frame=self.total_frames)
+
+        # Set interpolation to linear for constant rotation speed
+        for fcurve in self.ball.animation_data.action.fcurves:
+            for kf in fcurve.keyframe_points:
+                kf.interpolation = 'LINEAR'
 
 
     def get_screen_positions(self):
-        pass
+        ''' Returns the screen coords of the ball
+
+            This Funciton should return the screen coords of the ball, to use it in the ground truth file
+            The network should only get the ball-area as input
+
+            At the moment this only calculates the position in pixels.
+            Maybe it would be beneficial to also include the size of the ball ROI
+            -> This was buggy in the last implementation so just let it be a parameter for now
+
+        '''
+        center = bpy_extras.object_utils.world_to_camera_view(
+            scene=self.scene,
+            obj=self.scene.camera,
+            coord=self.ball.location
+        )
+        render = self.scene.render
+        res_x = render.resolution_x * render.resolution_percentage / 100
+        res_y = render.resolution_y * render.resolution_percentage / 100
+
+        pixel_x = center.x * res_x
+        pixel_y = (1 - center.y) * res_y
+
+        return pixel_x, pixel_y
 
 
     def update_ground_truth(self, frame):
-        pass
+        """ Update the ground truth file with the current frame's data
+
+            This function updates the pixel coords of the ball at the given frame.
+        """
+        
+        self.ball_coords.append((frame, self.get_screen_positions()))
 
 
     def save_ground_truth(self):
-        pass
+        """ Save the ground truth data to a file
+        """
+        # Save ground truth (rotation info) as CSV
+        gt = {
+            "rotation_x": [self.spin.get_axis()[0]],
+            "rotation_y": [self.spin.get_axis()[1]],
+            "rotation_z": [self.spin.get_axis()[2]],
+            "rotation_omega": [self.spin.get_angle()],
+        }
+        gt_df = pd.DataFrame(gt)
+        gt_df.to_csv(self.gt_path, index=False)
+        self.logger.debug(f"Ground truth data saved to {self.gt_path}")
+
+        # Save metadata as CSV
+        metadata = {
+            "rotation_x": [self.spin.get_axis()[0]],
+            "rotation_y": [self.spin.get_axis()[1]],
+            "rotation_z": [self.spin.get_axis()[2]],
+            "rotation_omega": [self.spin.get_angle()],
+            "ball_start_x": [self.ball_start [0]],
+            "ball_start_y": [self.ball_start[1]],
+            "ball_start_z": [self.ball_start[2]],
+            "ball_end_x": [self.ball_end[0]],
+            "ball_end_y": [self.ball_end[1]],
+            "ball_end_z": [self.ball_end[2]],
+            "initial_rot_x": [self.initial_orientation.get_axis()[0]],
+            "initial_rot_y": [self.initial_orientation.get_axis()[1]],
+            "initial_rot_z": [self.initial_orientation.get_axis()[2]],
+            "ball_position_world_x": [self.ball.location[0]],
+            "ball_position_world_y": [self.ball.location[1]],
+            "ball_position_world_z": [self.ball.location[2]],
+            "camera_position_world_x": [self.event_camera.cam.location[0]],
+            "camera_position_world_y": [self.event_camera.cam.location[1]],
+            "camera_position_world_z": [self.event_camera.cam.location[2]],
+            "camera_rotation_world_x": [self.event_camera.cam.rotation_euler[0]],
+            "camera_rotation_world_y": [self.event_camera.cam.rotation_euler[1]],
+            "camera_rotation_world_z": [self.event_camera.cam.rotation_euler[2]],
+            "total_frames": [self.total_frames],
+            "total_rotations": [self.total_rotations],
+            "video_length": [self.video_length],
+            "fps": [self.fps],
+        }
+        metadata_df = pd.DataFrame(metadata)
+        metadata_df.to_csv(self.metadata_path, index=False)
+        self.logger.debug(f"Metadata saved to {self.metadata_path}")
+
+        # Save ball coordinates per frame as CSV
+        coords_df = pd.DataFrame(self.ball_coords, columns=["frame", "screen_position"])
+        # Split screen_position tuple into two columns
+        coords_df[["screen_x", "screen_y"]] = pd.DataFrame(coords_df["screen_position"].tolist(), index=coords_df.index)
+        coords_df = coords_df.drop(columns=["screen_position"])
+        coords_df.to_csv(self.coords_path, index=False)
+        self.logger.debug(f"Ball coordinates saved to {self.coords_path}")
 
 
     def redirect_output(self):
-        pass
+        """ Redirect the blender output to a file
 
-    
+            This function redirects the blender output to a file, so that the logs can be saved
+            and used for debugging purposes.
+        """
+        logfile = self.dataset_path + f"tmp/pid_{self.pid}/render.log"
+        with open(logfile, 'a') as f:
+            f.write("\n\n========== NEW BLENDER OUTPUT ==========\n\n")
+            f.write(f"Simulation {self.simulation_nr} started at {time.strftime('%Y-%m-%d %H:%M:%S')}\n\n\n")
+        self.old = os.dup(sys.stdout.fileno())
+        sys.stdout.flush()
+        os.close(sys.stdout.fileno())
+        self.fd = os.open(logfile, os.O_WRONLY | os.O_APPEND)
+
+
     def restore_output(self):
-        pass
+        os.close(self.fd)
+        os.dup(self.old)
+        os.close(self.old)
 
 
     def simulate(self):
@@ -150,10 +314,13 @@ class Simulator:
 
             if frame % 100 == 0:
                 self.logger.progress(f"Simulation {self.simulation_nr}: Rendering frame {frame}/{self.scene.frame_end}  ({duration:.2f} s/frame, {int(duration*self.total_frames)}s total.)")
+            self.scene.frame_set(frame)
 
             self.scene.render.filepath = self.tmp_path
             bpy.ops.render.render(write_still=True)
             img = cv2.imread(self.tmp_path)
+
+            self.logger.debug(f"Ball Location: {self.ball.location}, Frame: {frame}, Image shape: {img.shape}")
 
             self.update_ground_truth(frame)
             if self.generate_video:
@@ -185,7 +352,9 @@ class Simulator:
         """
         self.init_scene()
         self.init_camera()
+        self.apply_initial_rotation()
         self.generate_spin_keyframes()
+        self.generate_scale_keyframes()
         self.generate_position_keyframes()
         self.simulate()
         self.logger.info(f"Simulation {self.simulation_nr} finished. Output saved to {self.output_name}")
@@ -193,3 +362,15 @@ class Simulator:
 
 if __name__ == "__main__":
     print("test simulator module")
+
+    import yaml
+    import logger
+
+    with open("configs/simulator/default.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    logger = logger.Logger(path="/data/lkolmar/datasets/test/tmp/")
+
+    sim = Simulator(config, logger, simulation_nr=0, pid=0)
+    sim.run_simulation()
+    print("Simulation finished.")
